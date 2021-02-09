6870163936f9dfe99d8e943a30e66fa94090977e
Kenneth Soh <kennethsohyq@gmail.com>
2021-02-09 17:27:13 +0800

    Renamed header

--- server/messagesocket/header.go
+++ server/messagesocket/header.go
@@ -18,7 +18,7 @@ func CreateErrorMessageHeader(lengthOfPayload uint16) []byte {
   return data
 }
 
-func CreateFacilityAvailabilityQueryHeader(dataLength uint16) []byte {
+func CreateQueryFactilityAvailabilityHeader(dataLength uint16) []byte {
   // Add 15 for type string length (1 byte), Availability string (12 bytes), Data String Length (2 bytes)
   s := "0000"+fmt.Sprintf("%04x", dataLength+15)+"0c417661696c6162696c697479"+fmt.Sprintf("%04x", dataLength)
   data, err := hex.DecodeString(s)
