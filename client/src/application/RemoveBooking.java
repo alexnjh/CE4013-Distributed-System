571b1c097bfb971dccc1a0b6be278620834cec2b
Kenneth Soh <kennethsohyq@gmail.com>
2021-03-07 14:15:27 +0800

    Cleaned up formatting for remove booking (#16)

--- client/src/application/RemoveBooking.java
+++ client/src/application/RemoveBooking.java
@@ -18,110 +18,110 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class RemoveBooking {
-private static ReplyMessage reply;
-	
-	
-	public static void showScene(Stage stage, Connection conn, String name){
-		GridPane rBook = new GridPane();
-		rBook.setPadding(new Insets(10, 10, 10, 10));
-		rBook.setVgap(8);// set vertical gap
-		rBook.setHgap(10);
-		Label conId = new Label("Enter ConfirmationID: ");
-		GridPane.setConstraints(conId, 0, 0);
-
-		// text field
-		TextField id = new TextField();
-		GridPane.setConstraints(id, 1, 0);
-		
-		Button rbutton = new Button("Delete");
-		Button cancel = new Button("Cancel");
-		
-		HBox.setMargin(conId, new Insets(0, 10, 0, 0));
-		rBook.add(new HBox(conId, id), 0,4);
-		
-		HBox.setMargin(rbutton, new Insets(0, 10, 0, 0));
-	    rBook.add(new HBox(rbutton, cancel), 0, 5);
-		
-		rBook.setAlignment(Pos.CENTER);
-		
-	    rbutton.setOnAction(e -> {
-
-			String cid = id.getText();
-
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setTitle("Error");
-			alert.setHeaderText(null);
+    private static ReplyMessage reply;
 
 
-			if(id.getText().isEmpty()) {
-				alert.setContentText("Invalid Confirmation ID");
-				alert.showAndWait();
-				return;
-			}
-			//sent the request to the server
-			ProgressForm pForm = new ProgressForm();
+    public static void showScene(Stage stage, Connection conn, String name) {
+        GridPane rBook = new GridPane();
+        rBook.setPadding(new Insets(10, 10, 10, 10));
+        rBook.setVgap(8);// set vertical gap
+        rBook.setHgap(10);
+        Label conId = new Label("Enter ConfirmationID: ");
+        GridPane.setConstraints(conId, 0, 0);
 
-			// Send message to server and wait for reply
-			Task<Void> task = new Task<>() {
-				@Override
-				public Void call() {
+        // text field
+        TextField id = new TextField();
+        GridPane.setConstraints(id, 1, 0);
 
-					// remove booking request
-					RemoveRequest req = new RemoveRequest(cid);
-					System.out.println("Request booking removal for " + cid);
+        Button rbutton = new Button("Delete");
+        Button cancel = new Button("Cancel");
 
-					updateProgress(1, 10);
-					try {
-						reply = conn.sendMessage(req.Marshal());
-						System.out.println("Await reply");
-					} catch (IOException e) {
-						// Generic error block
-						System.out.println(e.toString());
-					}
+        HBox.setMargin(conId, new Insets(0, 10, 0, 0));
+        rBook.add(new HBox(conId, id), 0, 4);
 
-					updateProgress(10, 10);
-					return null;
-				}
-			};
-			// binds progress of progress bars to progress of task:
-			pForm.activateProgressBar(task);
+        HBox.setMargin(rbutton, new Insets(0, 10, 0, 0));
+        rBook.add(new HBox(rbutton, cancel), 0, 5);
 
-			// in real life this method would get the result of the task
-			// and update the UI based on its value:
-			task.setOnSucceeded(event -> {
-				pForm.getDialogStage().close();
+        rBook.setAlignment(Pos.CENTER);
 
-				System.out.println(reply.getType());
+        rbutton.setOnAction(e -> {
 
-			  if (reply.getType().equals("Error")) {
-				Alert alert2 = new Alert(AlertType.ERROR);
-				alert2.setTitle("Server reply");
-				alert2.setHeaderText(null);
-				alert2.setContentText(new String(reply.getPayload(), StandardCharsets.UTF_8));
-				alert2.showAndWait();
-			}else if (reply.getType().equals("Confirm")){
-				// Else show the confirmation ID
-				//need to show the success dialog box??
-				Alert alert2 = new Alert(AlertType.INFORMATION);
-				alert2.setTitle("Remove Request");
-				alert2.setHeaderText(null);
-				String replyId = new String(reply.getPayload(), StandardCharsets.UTF_8);
-				alert2.setContentText("Booking successfully deleted!\n\nRef: " + replyId);
-				alert2.showAndWait();
-			}
-			  MenuScene.showScene(stage, conn, name);
-	  });
-			Thread thread = new Thread(task);
-			thread.start();
-	 });
-		    	
-		
-		cancel.setOnAction(e -> MenuScene.showScene(stage, conn, name));
-		
-		Scene scene = new Scene(rBook,400,400); 
-	    stage.setScene(scene);
-	    stage.show();
-	       
-	
-	}
+            String cid = id.getText();
+
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+
+
+            if (id.getText().isEmpty()) {
+                alert.setContentText("Invalid Confirmation ID");
+                alert.showAndWait();
+                return;
+            }
+            //sent the request to the server
+            ProgressForm pForm = new ProgressForm();
+
+            // Send message to server and wait for reply
+            Task<Void> task = new Task<>() {
+                @Override
+                public Void call() {
+
+                    // remove booking request
+                    RemoveRequest req = new RemoveRequest(cid);
+                    System.out.println("Request booking removal for " + cid);
+
+                    updateProgress(1, 10);
+                    try {
+                        reply = conn.sendMessage(req.Marshal());
+                        System.out.println("Await reply");
+                    } catch (IOException e) {
+                        // Generic error block
+                        System.out.println(e.toString());
+                    }
+
+                    updateProgress(10, 10);
+                    return null;
+                }
+            };
+            // binds progress of progress bars to progress of task:
+            pForm.activateProgressBar(task);
+
+            // in real life this method would get the result of the task
+            // and update the UI based on its value:
+            task.setOnSucceeded(event -> {
+                pForm.getDialogStage().close();
+
+                System.out.println(reply.getType());
+
+                if (reply.getType().equals("Error")) {
+                    Alert alert2 = new Alert(AlertType.ERROR);
+                    alert2.setTitle("Server reply");
+                    alert2.setHeaderText(null);
+                    alert2.setContentText(new String(reply.getPayload(), StandardCharsets.UTF_8));
+                    alert2.showAndWait();
+                } else if (reply.getType().equals("Confirm")) {
+                    // Else show the confirmation ID
+                    //need to show the success dialog box??
+                    Alert alert2 = new Alert(AlertType.INFORMATION);
+                    alert2.setTitle("Remove Request");
+                    alert2.setHeaderText(null);
+                    String replyId = new String(reply.getPayload(), StandardCharsets.UTF_8);
+                    alert2.setContentText("Booking successfully deleted!\n\nRef: " + replyId);
+                    alert2.showAndWait();
+                }
+                MenuScene.showScene(stage, conn, name);
+            });
+            Thread thread = new Thread(task);
+            thread.start();
+        });
+
+
+        cancel.setOnAction(e -> MenuScene.showScene(stage, conn, name));
+
+        Scene scene = new Scene(rBook, 400, 400);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
 }
--- client/src/application/RemoveRequest.java
+++ client/src/application/RemoveRequest.java
@@ -1,23 +1,23 @@
 package application;
+
 //sent the confirmation id to the server
-public class RemoveRequest implements RequestMessage{
-		
-		private String cid;
-		
-		
-		RemoveRequest(String id){
-			this.cid = id;
-		}
-		
-		@Override
-		public byte[] Marshal() {
+public class RemoveRequest implements RequestMessage {
 
-			byte[] payload = cid.getBytes();
-			byte[] header = Header.CreateRemoveBookingHeader(payload.length);
-			byte[] finalPayload = Helper.ConcatByteArray(header,payload);
-			
-			return finalPayload;
-		}
+    private String cid;
 
-	}
+    RemoveRequest(String id) {
+        this.cid = id;
+    }
+
+    @Override
+    public byte[] Marshal() {
+
+        byte[] payload = cid.getBytes();
+        byte[] header = Header.CreateRemoveBookingHeader(payload.length);
+        byte[] finalPayload = Helper.ConcatByteArray(header, payload);
+
+        return finalPayload;
+    }
+
+}
 
