f5d32f4ae1eae5a56f9a2023596c2b864b616fba
Kenneth Soh <kennethsohyq@gmail.com>
2021-03-07 13:46:01 +0800

    Cleaned up some of the remove booking code (#16)

--- client/src/application/RemoveBooking.java
+++ client/src/application/RemoveBooking.java
@@ -1,23 +1,21 @@
 package application;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
 import application.NewBookingScene.ProgressForm;
 import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class RemoveBooking {
 private static ReplyMessage reply;
@@ -46,77 +44,75 @@ private static ReplyMessage reply;
 		
 		rBook.setAlignment(Pos.CENTER);
 		
-	    rbutton.setOnAction(new EventHandler<ActionEvent>() {
-		    @Override public void handle(ActionEvent e) {
-		    	
-		    	String cid =(String)id.getText();
-		    	
-		    	Alert alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Error");
-				alert.setHeaderText(null);
-				
-								
-				if(id.getText().isEmpty()) {
-					alert.setContentText("Invalid Confirmation ID");
-					alert.showAndWait();
-					return;
+	    rbutton.setOnAction(e -> {
+
+			String cid = id.getText();
+
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText(null);
+
+
+			if(id.getText().isEmpty()) {
+				alert.setContentText("Invalid Confirmation ID");
+				alert.showAndWait();
+				return;
+			}
+			//sent the request to the server
+			ProgressForm pForm = new ProgressForm();
+
+			// Send message to server and wait for reply
+			Task<Void> task = new Task<>() {
+				@Override
+				public Void call() {
+
+					// remove booking request
+					RemoveRequest req = new RemoveRequest(cid);
+					System.out.println("Request booking removal for " + cid);
+
+					updateProgress(1, 10);
+					try {
+						reply = conn.sendMessage(req.Marshal());
+						System.out.println("Await reply");
+					} catch (IOException e) {
+						// Generic error block
+						System.out.println(e.toString());
+					}
+
+					updateProgress(10, 10);
+					return null;
 				}
-		    	//sent the request to the server           	
-		    	ProgressForm pForm = new ProgressForm();
-				
-	            // Send message to server and wait for reply
-	            Task<Void> task = new Task<Void>() {
-	                @Override
-	                public Void call() throws InterruptedException {
-	                	
-	                	// remove booking request
-	                	RemoveRequest req = new RemoveRequest(cid);
-						System.out.println("Request booking removal for " + cid);;
-	                	
-	                	updateProgress(1, 10);
-	                	try {
-							reply = conn.sendMessage(req.Marshal());
-							System.out.println("Await reply");
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							System.out.println(e.toString());
-						}
-	                	
-	                    updateProgress(10, 10);
-	                    return null ;
-	                }
-	            };
-	            // binds progress of progress bars to progress of task:
-	            pForm.activateProgressBar(task);
-	            
-	            // in real life this method would get the result of the task
-	            // and update the UI based on its value:
-	            task.setOnSucceeded(event -> {
-	            	pForm.getDialogStage().close();
-	            	
-	            	System.out.println(reply.getType());
-		    	
-		      	if (reply.getType().equals("Error")) {
-					Alert alert2 = new Alert(AlertType.ERROR);
-					alert2.setTitle("Server reply");
-					alert2.setHeaderText(null);
-					alert2.setContentText(new String(reply.getPayload(), StandardCharsets.UTF_8));
-					alert2.showAndWait();		
-            	}else if (reply.getType().equals("Confirm")){
-            		// Else show the confirmation ID
-            		//need to show the success dialog box??
-					Alert alert2 = new Alert(AlertType.INFORMATION);
-					alert2.setTitle("Remove Request");
-					alert2.setHeaderText(null);
-					String replyId = new String(reply.getPayload(), StandardCharsets.UTF_8);
-					alert2.setContentText("Booking successfully deleted!\n\nRef: " + replyId);
-					alert2.showAndWait();	
-            	}	
-		      	MenuScene.showScene(stage, conn, name);
-	      });
-				Thread thread = new Thread(task);
-				thread.start();
-	     }
+			};
+			// binds progress of progress bars to progress of task:
+			pForm.activateProgressBar(task);
+
+			// in real life this method would get the result of the task
+			// and update the UI based on its value:
+			task.setOnSucceeded(event -> {
+				pForm.getDialogStage().close();
+
+				System.out.println(reply.getType());
+
+			  if (reply.getType().equals("Error")) {
+				Alert alert2 = new Alert(AlertType.ERROR);
+				alert2.setTitle("Server reply");
+				alert2.setHeaderText(null);
+				alert2.setContentText(new String(reply.getPayload(), StandardCharsets.UTF_8));
+				alert2.showAndWait();
+			}else if (reply.getType().equals("Confirm")){
+				// Else show the confirmation ID
+				//need to show the success dialog box??
+				Alert alert2 = new Alert(AlertType.INFORMATION);
+				alert2.setTitle("Remove Request");
+				alert2.setHeaderText(null);
+				String replyId = new String(reply.getPayload(), StandardCharsets.UTF_8);
+				alert2.setContentText("Booking successfully deleted!\n\nRef: " + replyId);
+				alert2.showAndWait();
+			}
+			  MenuScene.showScene(stage, conn, name);
+	  });
+			Thread thread = new Thread(task);
+			thread.start();
 	 });
 		    	
 		
